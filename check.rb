b1b763649d15f4e1359e267714f69dbde4f1553b
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2016-04-06 11:52:43 +0300

    v3.3.6.1

--- check.rb
+++ check.rb
@@ -13,16 +13,18 @@ require 'net/http'
 
 load 'lib/bootswatch-sass/version.rb'
 
-json = Net::HTTP.get(URI.parse('http://api.bootswatch.com/3/'))
+json = Net::HTTP.get(URI.parse('https://api.github.com/repos/thomaspark/bootswatch/tags'))
 json = JSON.parse(json)
+tag = json.select { |tag| tag['name'] =~ /^v\d+\.\d+\.\d+/ }.first
 
 local_version = Gem::Version.new(BootswatchSass::VERSION)
-remote_version = Gem::Version.new(json['version'].gsub('+', '.'))
+remote_version_string = tag['name'].gsub(/^v/, '')
+remote_version = Gem::Version.new(remote_version_string.gsub('+', '.'))
 
 if local_version < remote_version
-  puts "Local version #{local_version} is behind remote version #{json['version']} :-("
+  puts "Local version #{local_version} is behind remote version #{remote_version_string} :-("
 elsif local_version > remote_version
-  puts "Local version #{local_version} is ahead of remote version #{json['version']} 0.o"
+  puts "Local version #{local_version} is ahead of remote version #{remote_version_string} 0.o"
 else
-  puts "Local version #{local_version} is the same as remote version #{json['version']} :-)"
+  puts "Local version #{local_version} is the same as remote version #{remote_version_string} :-)"
 end
